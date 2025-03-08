41f75070ce2674e1107e69a2caa888724006d0fe
jayawickrama <nishantharaka2004@gmail.com>
2025-03-08 10:03:00 +0530

    application started success

--- Server.ts
+++ Server.ts
@@ -2,13 +2,20 @@ import express from "express"
 const app =express()
 const dotenv = require('dotenv');
 import cors from 'cors'
+import mainRouter from "./Real-Time-Code-Editor/Routers/MainRouter";
 dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use('/api/v1',mainRouter.router)
 app.use(cors({
     origin: "http://localhost:5173",
     methods: ['GET','POST','PUT','PATCH','DELETE'],
     allowedHeaders: ['Content-Type','Authorization'],
     credentials: true,
 }));
+const Port =process.env.PORT
+app.listen(Port,()=>{
+    console.log("Application started on port :",Port)
+})
